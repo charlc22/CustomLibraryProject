ae4b30a8a9dd5c925018f7515fa524032ea030d6
charlescetta <charlescetta@outlook.com>
2024-02-02 20:35:35 -0500

    1st Commit

---
+++ CustomLibrary project/Class1.cs
@@ -0,0 +1,25 @@
+﻿/*Name: Charles Cetta
+ Course: CPS 3330-02
+ Lab: #2
+ Kean ID#: 1193276
+*/
+
+namespace CustomLibrary_project;
+
+public class Average
+{
+    public static double MyAverage(double num1, double num2)
+    {
+       
+        return (num1 + num2) / 2;
+        
+    }
+
+    public static double MyAverage(double num1, double num2, double num3)
+    {
+       
+        return (num1 + num2 + num3) / 3;
+       
+    }
+}
+
---
+++ TestCustomLibrary/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Name: Charles Cetta
+ Course: CPS 3330-02
+ Lab: #2
+ Kean ID#: 1193276
+*/
+
+using static System.Console;
+
+using CustomLibrary_project;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //ask user for two nums
+        Console.Write("Enter the first number: ");
+        double num1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter the second number: ");
+        double num2 = Convert.ToDouble(Console.ReadLine());
+
+        //Avg of two nums
+        double twoave = Average.MyAverage(num1, num2);
+        Console.WriteLine($"The average of the two numbers is: {twoave}");
+
+        // Additional num
+        Console.Write("Enter an additional number: ");
+        double num3 = Convert.ToDouble(Console.ReadLine());
+
+        //Avg of three nums
+        double threeave = Average.MyAverage(num1, num2, num3);
+        Console.WriteLine($"The average of the three numbers is: {threeave}");
+
+    }
+}
